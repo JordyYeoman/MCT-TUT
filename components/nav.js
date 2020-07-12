37fa614ce244ff830ae732e2792165622d5271a8
JordyYeoman <jordyyeoman@gmail.com>
2020-07-12 15:28:53 +0800

    first commit

---
+++ components/nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Nav = () => {
+    return (
+        <div className="Nav-Container">Hello
+        <style jsx>{`
+        .Nav-Container {
+            width: 100%;
+            border-bottom: 1px solid light-blue;
+        }
+      `}</style>
+        </div>
+    )
+}
+
+
+export default Nav;
